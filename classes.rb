c7849b377917f9a2e1c474dc6b7ab91e0c574bd9
mikhail-sukhodolov <82030194+mikhail-sukhodolov@users.noreply.github.com>
2021-10-27 08:58:03 +0300

    Update classes.rb

--- classes.rb
+++ classes.rb
@@ -109,7 +109,7 @@ class Train
 
   
   def move_forward
-    self.station = next_station if next_station != self.route.station_list.last
+    self.station = next_station if next_station != nil
   end
 
   def move_back
